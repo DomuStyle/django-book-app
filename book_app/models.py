9f74dff646ec6ded069af07842a1e96c054a3c45
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-05-23 09:34:31 +0200

    added: BookSerializer

--- book_app/models.py
+++ book_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    title = models.CharField(max_length=50, default="No title defined")
+    author = models.CharField(max_length=25, default="No author defined")
+    price = models.FloatField()
